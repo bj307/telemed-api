509bb57c0e29e524fd6a9363694d6f50bfb6fbe6
kaio <kaioresendeoficial@outlook.com>
2023-12-17 11:41:51 -0300

    :sparkles: feat: login paciente ok

--- src/session/session.module.ts
+++ src/session/session.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SessionService } from './session.service';
 import { SessionController } from './session.controller';
 import { AdmService } from 'src/adm/adm.service';
+import { PacienteService } from 'src/paciente/paciente.service';
 
 @Module({
   controllers: [SessionController],
-  providers: [SessionService, AdmService],
+  providers: [SessionService, AdmService, PacienteService],
 })
 export class SessionModule {}
