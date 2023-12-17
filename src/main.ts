d78557a4ba9b9a511e7f0c25c52b844c3ed3fe92
joaovitorpagani <joaovitorpagani7@hotmail.com>
2023-12-17 18:18:26 -0300

    :Deploy: exceçao para fire connect na main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,16 @@ async function bootstrap() {
   dotenv.config();
 
   const serviceAccount = JSON.parse(process.env.FIRE_CONNECT);
+  
+  if (!serviceAccount) {
+    throw new Error('FIRE_CONNECT is not defined');
+  }
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
-
+  
+  
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 3000;
