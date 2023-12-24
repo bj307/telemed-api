5957ac48687f5e3bb625893e07213d94d7cc7d83
kaio <kaioresendeoficial@outlook.com>
2023-12-24 20:19:28 -0300

    :bug: fix: erro findAll pacientes corrigido

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,37 +1,50 @@
+/* eslint-disable prettier/prettier */
 import { Server, Socket } from 'socket.io';
-import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { ChatService } from "./chat.service";
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { ChatService } from './chat.service';
 import { Chat } from './dto/chat.dto';
 import { OnModuleInit } from '@nestjs/common';
 
 @WebSocketGateway()
-export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect, OnModuleInit {
-    
-    @WebSocketServer()
-    server: Server;
+export class ChatGateway
+  implements OnGatewayConnection, OnGatewayDisconnect, OnModuleInit
+{
+  @WebSocketServer()
+  server: Server;
 
-    constructor(private chatService: ChatService) { }
+  constructor(private chatService: ChatService) {}
 
-    onModuleInit() {
-        console.log('ChatGateway iniciado');
-    }
+  onModuleInit() {
+    console.log('ChatGateway iniciado');
+  }
 
-    afterInit(server: Server) {
-        console.log('ChatGateway inicializado');
-        this.server = server;
-    }
+  afterInit(server: Server) {
+    console.log('ChatGateway inicializado');
+    this.server = server;
+  }
 
-    handleConnection(client: Socket) {
-        console.log('Cliente conectado');
-    }
+  handleConnection(client: Socket) {
+    console.log('Cliente conectado');
+  }
 
-    handleDisconnect(client: Socket) {
-        console.log('Cliente desconectado');
-    }
+  handleDisconnect(client: Socket) {
+    console.log('Cliente desconectado');
+  }
 
-    @SubscribeMessage('chat')
-    async handleNewMessage(@MessageBody() chat: Chat, @ConnectedSocket() sender: Socket) {
-        console.log('novo chat', chat);
-        sender.emit('novo chat', chat);
-    }
+  @SubscribeMessage('chat')
+  async handleNewMessage(
+    @MessageBody() chat: Chat,
+    @ConnectedSocket() sender: Socket,
+  ) {
+    console.log('novo chat', chat);
+    sender.emit('novo chat', chat);
+  }
 }
